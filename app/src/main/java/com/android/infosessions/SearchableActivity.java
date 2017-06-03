d8fce7b48cec037c5d3c5c609f13cdb0e1f2dadc
ThaoTrann <6.28.tran@gmail.com>
2017-06-02 23:30:11 -0700

    Add a filter shower to searchable activity
    Whenever a new audience is selected or removed, update the shower

--- app/src/main/java/com/android/infosessions/SearchableActivity.java
+++ app/src/main/java/com/android/infosessions/SearchableActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.database.Cursor;
 import android.app.LoaderManager;
 import android.content.Loader;
+import android.graphics.Color;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +23,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.HorizontalScrollView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -41,6 +43,11 @@ public class SearchableActivity extends AppCompatActivity implements android.wid
     private String mQuery = "";
     private ListView sessionsListView;
     private TextView textView;
+    HorizontalScrollView hsv;
+    private LinearLayout filterTabsLL;
+
+    private static final int SESSION_LOADER = 0;
+    private static final int FILTER_LOADER = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,9 +55,14 @@ public class SearchableActivity extends AppCompatActivity implements android.wid
         setContentView(R.layout.sessions_list);
         setTitle("Search");
         sessionsListView = (ListView) findViewById(R.id.list);
+
         View emptyView = findViewById(R.id.empty_view);
         emptyView.setVisibility(View.VISIBLE);
         sessionsListView.setEmptyView(emptyView);
+
+        hsv = (HorizontalScrollView) findViewById(R.id.filter_hsv);
+        hsv.setVisibility(View.VISIBLE);
+        filterTabsLL = (LinearLayout) findViewById(R.id.filter_ll);
 
         textView = (TextView) findViewById(R.id.text);
         textView.setVisibility(View.VISIBLE);
@@ -74,13 +86,15 @@ public class SearchableActivity extends AppCompatActivity implements android.wid
             }
         });
 
-        getLoaderManager().initLoader(0, null, this);
+        getLoaderManager().initLoader(SESSION_LOADER, null, this);
+        getLoaderManager().initLoader(FILTER_LOADER, null, this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        getLoaderManager().restartLoader(0, null, this);
+        getLoaderManager().restartLoader(SESSION_LOADER, null, this);
+        getLoaderManager().restartLoader(FILTER_LOADER, null, this);
     }
 
     @Override
@@ -118,91 +132,145 @@ public class SearchableActivity extends AppCompatActivity implements android.wid
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        String[] projection = {
-                SessionEntry._ID,
-                SessionEntry.COLUMN_SESSION_EMPLOYER,
-                SessionEntry.COLUMN_SESSION_START_TIME,
-                SessionEntry.COLUMN_SESSION_END_TIME,
-                SessionEntry.COLUMN_SESSION_DATE,
-                SessionEntry.COLUMN_SESSION_DAY,
-                SessionEntry.COLUMN_SESSION_MILLISECONDS,
-                SessionEntry.COLUMN_SESSION_WEBSITE,
-                SessionEntry.COLUMN_SESSION_LINK,
-                SessionEntry.COLUMN_SESSION_DESCRIPTION,
-                SessionEntry.COLUMN_SESSION_BUILDING_CODE,
-                SessionEntry.COLUMN_SESSION_BUILDING_NAME,
-                SessionEntry.COLUMN_SESSION_BUILDING_ROOM,
-                SessionEntry.COLUMN_SESSION_MAP_URL,
-                SessionEntry.COLUMN_SESSION_LOGO,
-                SessionEntry.COLUMN_SESSION_AUDIENCE};
+        switch (id) {
+            case SESSION_LOADER:
 
-        if(mQuery.trim().isEmpty()) {
-            mQuery = "";
+                String[] projection = {
+                        SessionEntry._ID,
+                        SessionEntry.COLUMN_SESSION_EMPLOYER,
+                        SessionEntry.COLUMN_SESSION_START_TIME,
+                        SessionEntry.COLUMN_SESSION_END_TIME,
+                        SessionEntry.COLUMN_SESSION_DATE,
+                        SessionEntry.COLUMN_SESSION_DAY,
+                        SessionEntry.COLUMN_SESSION_MILLISECONDS,
+                        SessionEntry.COLUMN_SESSION_WEBSITE,
+                        SessionEntry.COLUMN_SESSION_LINK,
+                        SessionEntry.COLUMN_SESSION_DESCRIPTION,
+                        SessionEntry.COLUMN_SESSION_BUILDING_CODE,
+                        SessionEntry.COLUMN_SESSION_BUILDING_NAME,
+                        SessionEntry.COLUMN_SESSION_BUILDING_ROOM,
+                        SessionEntry.COLUMN_SESSION_MAP_URL,
+                        SessionEntry.COLUMN_SESSION_LOGO,
+                        SessionEntry.COLUMN_SESSION_AUDIENCE};
+
+                if(mQuery.trim().isEmpty()) {
+                    mQuery = "";
+                }
+
+                String[] p = {
+                        FilterEntry._ID,
+                        FilterEntry.COLUMN_FILTER_KEY,
+                        FilterEntry.COLUMN_FILTER_IS_CODE,
+                        FilterEntry.COLUMN_FILTER_VALUE};
+
+                String[] a = { String.valueOf(FilterEntry.VALUE_CHECKED), String.valueOf(FilterEntry.VALUE_NOT_CODE) };
+
+                Cursor filterCursor = getContentResolver().query(FilterEntry.CONTENT_URI, p,
+                        FilterEntry.COLUMN_FILTER_VALUE + "=? AND " + FilterEntry.COLUMN_FILTER_IS_CODE + "=?",
+                        a, null);
+                ArrayList<String> filters = new ArrayList<>();
+
+                String selection = SessionEntry.COLUMN_SESSION_EMPLOYER + " LIKE ? ";
+                filterCursor.moveToFirst();
+                int count = filterCursor.getCount();
+                if(count > 0) {
+                    selection += " AND (";
+                }
+                for(int i = 0; i < count; i++) {
+                    String audience = filterCursor.getString(filterCursor.getColumnIndexOrThrow(FilterEntry.COLUMN_FILTER_KEY));
+                    filters.add(audience);
+                    Log.d("LOG_TAG: filter ", "" + audience);
+                    if(i != 0) {
+                        selection += " OR ";
+                    }
+                    selection += SessionEntry.COLUMN_SESSION_AUDIENCE + " LIKE ? ";
+                    filterCursor.moveToNext();
+                }
+                if(count > 0) {
+                    selection += ")";
+                }
+                filterCursor.close();
+
+                String[] selectionArgs = new String[1+filters.size()];
+                selectionArgs[0] = ("%" + mQuery + "%") ;
+                for(int i = 1; i < selectionArgs.length; i++) {
+                    selectionArgs[i] = ("%" + filters.get(i-1) + "%");
+                }
+                Log.d("LOG_TAG: selection", "" + selection);
+                return new CursorLoader(this,
+                        SessionEntry.CONTENT_URI,
+                        projection,
+                        selection,
+                        selectionArgs,
+                        null);
+            case FILTER_LOADER:
+
+                String[] proj = {
+                        FilterEntry._ID,
+                        FilterEntry.COLUMN_FILTER_KEY,
+                        FilterEntry.COLUMN_FILTER_IS_CODE,
+                        FilterEntry.COLUMN_FILTER_VALUE};
+
+                String[] argus = { String.valueOf(FilterEntry.VALUE_CHECKED), String.valueOf(FilterEntry.VALUE_NOT_CODE) };
+                return new CursorLoader(this,
+                        FilterEntry.CONTENT_URI,
+                        proj,
+                        FilterEntry.COLUMN_FILTER_VALUE + "=? AND " + FilterEntry.COLUMN_FILTER_IS_CODE + "=?",
+                        argus,
+                        null);
+            default:
+                return null;
         }
-
-        String[] p = {
-                FilterEntry._ID,
-                FilterEntry.COLUMN_FILTER_KEY,
-                FilterEntry.COLUMN_FILTER_IS_CODE,
-                FilterEntry.COLUMN_FILTER_VALUE};
-
-        String[] a = { String.valueOf(FilterEntry.VALUE_CHECKED), String.valueOf(FilterEntry.VALUE_NOT_CODE) };
-
-        Cursor filterCursor = getContentResolver().query(FilterEntry.CONTENT_URI, p,
-                FilterEntry.COLUMN_FILTER_VALUE + "=? AND " + FilterEntry.COLUMN_FILTER_IS_CODE + "=?",
-                a, null);
-        ArrayList<String> filters = new ArrayList<>();
-
-        String selection = SessionEntry.COLUMN_SESSION_EMPLOYER + " LIKE ? ";
-        filterCursor.moveToFirst();
-        int count = filterCursor.getCount();
-        if(count > 0) {
-            selection += " AND (";
-        }
-        for(int i = 0; i < count; i++) {
-            String audience = filterCursor.getString(filterCursor.getColumnIndexOrThrow(FilterEntry.COLUMN_FILTER_KEY));
-            filters.add(audience);
-            Log.d("LOG_TAG: filter ", "" + audience);
-            if(i != 0) {
-                selection += " OR ";
-            }
-            selection += SessionEntry.COLUMN_SESSION_AUDIENCE + " LIKE ? ";
-            filterCursor.moveToNext();
-        }
-        if(count > 0) {
-            selection += ")";
-        }
-        filterCursor.close();
-
-        String[] selectionArgs = new String[1+filters.size()];
-        selectionArgs[0] = ("%" + mQuery + "%") ;
-        for(int i = 1; i < selectionArgs.length; i++) {
-            selectionArgs[i] = ("%" + filters.get(i-1) + "%");
-        }
-        Log.d("LOG_TAG: selection", "" + selection);
-        return new CursorLoader(this,
-                SessionEntry.CONTENT_URI,
-                projection,
-                selection,
-                selectionArgs,
-                null);
     }
 
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mCursorAdapter.swapCursor(data);
+        switch (loader.getId()) {
+            case SESSION_LOADER:
+                mCursorAdapter.swapCursor(data);
+                break;
+            case FILTER_LOADER:
+                updateTabs(data);
+                break;
+            default:
+                return;
+        }
     }
 
+    public void updateTabs(Cursor cursor) {
+        cursor.moveToFirst();
+        hsv.removeAllViews();
+        filterTabsLL.removeAllViews();
+        while (!cursor.isAfterLast()) {
+            TextView tv = new TextView(this);
+            String key = cursor.getString(cursor.getColumnIndexOrThrow(FilterEntry.COLUMN_FILTER_KEY));
+            tv.setText(key);
+            tv.setTextColor(Color.WHITE);
+            tv.setPadding(40, 0, 40, 0);
+            filterTabsLL.addView(tv);
+            cursor.moveToNext();
+        }
+        hsv.addView(filterTabsLL);
+    }
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        mCursorAdapter.swapCursor(null);
+        switch (loader.getId()) {
+            case SESSION_LOADER:
+                mCursorAdapter.swapCursor(null);
+                break;
+            case FILTER_LOADER:
+                break;
+            default:
+                return;
+        }
     }
 
     @Override
     public boolean onQueryTextChange(String newText) {
         mQuery = newText;
-        getLoaderManager().restartLoader(0, null, this);
+        getLoaderManager().restartLoader(SESSION_LOADER, null, this);
+        getLoaderManager().restartLoader(FILTER_LOADER, null, this);
         return false;
     }
 
